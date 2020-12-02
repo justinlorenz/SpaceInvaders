e79c3324e490c0e5f91c6b8fce34055bfc756059
Justin Lorenz <jml166@duke.edu>
2020-12-02 15:28:32 -0600

    Starting my first python project

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+import time
+import random
+
+BLUE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+
+
---
+++ src/SpriteHandler.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+# Enemies
+BLUE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+RED_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+
+# Main Player
+YELLOW_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
